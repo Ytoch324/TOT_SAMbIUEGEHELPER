983c4d277fe3d7e0c182b634d6a287a307f4f8d7
Ytoch324 <115603703+Ytoch324@users.noreply.github.com>
2026-03-08 01:44:10 +0500

    Update bot_miniapp.py

--- bot_miniapp.py
+++ bot_miniapp.py
@@ -1,33 +1,26 @@
 import os
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, WebAppInfo
-from telegram.ext import Application, CommandHandler, ContextTypes
-import logging
-
-logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
+import telebot
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 WEBAPP_URL = "https://ytoch324.github.io/TOT_SAMbIUEGEHELPER/"  # вставь свою ссылку
 
+bot = telebot.TeleBot(BOT_TOKEN)
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    kb = InlineKeyboardMarkup([[
-        InlineKeyboardButton(
-            "📖 Открыть материалы",
-            web_app=WebAppInfo(url=WEBAPP_URL)
-        )
-    ]])
-    await update.message.reply_text(
+
+@bot.message_handler(commands=["start"])
+def start(message):
+    kb = InlineKeyboardMarkup()
+    kb.add(InlineKeyboardButton(
+        "📖 Открыть материалы",
+        web_app=WebAppInfo(url=WEBAPP_URL)
+    ))
+    bot.send_message(
+        message.chat.id,
         "Привет! Здесь теория и тесты для подготовки к заданиям 10 и 11 ЕГЭ по русскому языку.",
         reply_markup=kb
     )
 
 
-def main():
-    app = Application.builder().token(BOT_TOKEN).build()
-    app.add_handler(CommandHandler("start", start))
-    print("Бот запущен...")
-    app.run_polling(drop_pending_updates=True)
-
-
-if __name__ == "__main__":
-    main()
+print("Бот запущен...")
+bot.infinity_polling()
